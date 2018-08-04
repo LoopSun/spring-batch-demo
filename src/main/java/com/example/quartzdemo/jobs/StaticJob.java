ed2f5ae2a84b8ca05b245c59fc6f6e374fc722e5
loopsun <zhouhua@richinfo.cn>
2018-08-04 12:27:05 +0800

    Point: Spring Boot Schedule demo

---
+++ src/main/java/com/example/quartzdemo/jobs/StaticJob.java
@@ -0,0 +1,39 @@
+package com.example.quartzdemo.jobs;
+
+import com.example.quartzdemo.services.SimpleHelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+//SpringBoot自带的简单定时任务执行
+@Component
+public class StaticJob {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final static long SECOND = 1000;
+
+    @Autowired
+    SimpleHelloService simpleHelloService;
+
+//    固定每隔10s执行一次
+    @Scheduled(fixedRate = 10 * SECOND)
+    public void fixedRateJob() {
+        logger.info("{}", simpleHelloService.hello("fixedRateJob"));
+    }
+
+//    固定延迟10s执行一次
+    @Scheduled(fixedDelay = 10 * SECOND)
+    public void fixedDelayJob() {
+        logger.info("{}", simpleHelloService.hello("fixedDelayJob"));
+    }
+
+//    crontab方式定义更灵活的定时方式
+    @Scheduled(cron = "*/10 * * * * *")
+    public void cronJob() {
+        logger.info("{}", simpleHelloService.hello("cronJob"));
+    }
+}
---
+++ src/main/java/com/example/quartzdemo/services/SimpleHelloService.java
@@ -0,0 +1,16 @@
+package com.example.quartzdemo.services;
+
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class SimpleHelloService {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    public String hello(String name) {
+        return dateFormat.format(new Date()) + " Hello " + name;
+    }
+}
